f16ee1b45bd5836cb67760c5f600e4a8c388dcdf
eitan25 <eitan9994@gmail.com>
2022-08-30 12:00:18 +0300

    added giraffe and collections

--- Classes.cs
+++ Classes.cs
@@ -4,7 +4,7 @@ namespace Classes
 {
 	using System;
 	using System.Collections;
-	using System.Drawing.Imaging;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using static System.Math;
 
@@ -18,6 +18,8 @@ namespace Classes
 		float radius;
 		float speed;
 		bool isHunter;
+		bool is_hunted;     //if the lion "eats" the hunted animel the flag changes to true, and the animel deleted.
+
 
 		public float MOVE_X
         {
@@ -54,7 +56,14 @@ namespace Classes
 			set { isHunter = value; }
 		}
 
-		public abstract void Move(Hunted a = null);					//the animel moves
+		public bool IS_HUNTED
+		{
+			get { return is_hunted; }
+			set { is_hunted = value; }
+		}
+
+		public abstract void Move(Animel a = null);                 //the animel moves
+		public abstract void Draw(Graphics g);
 	}
 
 	//Base inherited abstruct class
@@ -93,7 +102,7 @@ namespace Classes
 			SPEED = rest;
 		}
 
-		public void Draw(Graphics g)
+		public override void Draw(Graphics g)
         {
 			g.DrawIcon(icon,(int)X,(int)Y);
         }
@@ -113,7 +122,7 @@ namespace Classes
 		/// <summary>
 		/// search for a hunted animel object, when allocated - move towword it. 
 		/// </summary>
-        public override void Move(Hunted a = null)
+        public override void Move(Animel a = null)
         {
 			//change move_x and move_y according to need.
 			if (SPEED == rest || a==null)		/////////////////////////////addad expression a==null in case that there is no object to hunt
@@ -125,7 +134,7 @@ namespace Classes
 			double x_val = (a.X - this.X);
 			double y_val = (a.Y - this.Y);
 			dist = Sqrt(x_val * x_val + y_val * y_val);
-			steps = (int)(dist / SPEED) + 1;
+			steps = (int)(dist / SPEED);
 			this.MOVE_X = (a.X - this.X) / steps;
 			this.MOVE_Y = (a.Y - this.Y) / steps;
 			//move toward the hunted animel
@@ -133,7 +142,7 @@ namespace Classes
 			Y = Y + MOVE_Y;
 
 			//if hunted change the flag of the animel to true
-			if (dist < (RADIUS))	////////////////////changed radius to this.radius from a.radius
+			if (dist <= (RADIUS))	////////////////////changed radius to this.radius from a.radius
 			{
 				a.IS_HUNTED = true;
 			}
@@ -144,24 +153,37 @@ namespace Classes
 	//Base inherited abstruct class
 	public abstract class Hunted : Animel
 	{
-		bool is_hunted;		//if the lion "eats" the hunted animel the flag changes to true, and the animel deleted.
+		float distance_from_lion;
 
 		public Hunted()
         {
 			ISHUNTER = false;
         }
 
-		public bool IS_HUNTED
+		public float DISTANCE_FROM_LION
         {
-            get { return is_hunted; }
-			set { is_hunted = value; }
+            get { return distance_from_lion; }
+            set { distance_from_lion = value; }
         }
+
+        /// <summary>
+        /// calculate the distance from the lion and sets it the the proper property.
+        /// </summary>
+        /// <param name="a">Lion</param>
+        public void SetDistFromLion(Animel a)
+		{
+			double dist;
+			double x_val = (a.X - this.X);
+			double y_val = (a.Y - this.Y);
+			dist = Sqrt(x_val * x_val + y_val * y_val);
+			DISTANCE_FROM_LION = (float)dist;
+		}
 
 	}
 
 	//rabbit inherited class
 	public class Rabbit : Hunted
-    {
+	{
 		const float radius = 10;
 		/////////////////addad icon
 		static Size s = new Size((int)radius * 2, (int)radius * 2);
@@ -180,24 +202,179 @@ namespace Classes
 			Y = yVal;
 			RADIUS = radius;
 			IS_HUNTED = false;
+			DISTANCE_FROM_LION = 0;
 		}
 
-		public void Draw(Graphics g)
+		public override void Draw(Graphics g)
 		{
 			g.DrawIcon(icon, (int)X, (int)Y);
 		}
 
 		/// <summary>
-		/// moving forword and chage direction when hits a wall.
+		/// moving forword and change direction when hits a wall.
+		/// saves the distance between the lion and the animel in each step of the animel.
 		/// </summary>
-		public override void Move(Hunted a = null)
+		/// <param name="a">Lion</param>
+		public override void Move(Animel a)
 		{
 			X = X + MOVE_X;
 			Y = Y + MOVE_Y;
-			if (X >= 1000-radius || X <= 0+radius)
+			if (X >= 1000-radius*2 || X <= 0+radius*2)
 				MOVE_X = -MOVE_X;
-			if (Y >= 500-radius || Y <= 0+radius)
+			if (Y >= 500-radius*2 || Y <= 0+radius*2)
 				MOVE_Y = -MOVE_Y;
-        }
+			//calculate distance
+			SetDistFromLion(a);
+		}
     }
+
+	//Giraffe inherited class
+	public class Giraffe : Hunted
+    {
+		const float radius = 35;
+		/////////////////addad icon
+		static Size s = new Size((int)radius * 2, (int)radius * 2);
+		static string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+		static Icon icon = new Icon(path + "\\Pictures\\giraffe.ico", s);
+
+		public Giraffe() : this(0, 0) { }
+
+		public Giraffe(float xVal, float yVal)
+		{
+			SPEED = 3;
+			Random rnd = new Random();
+			MOVE_X = rnd.Next(-(int)SPEED, (int)SPEED);
+			MOVE_Y = (float)Sqrt(SPEED * SPEED + MOVE_X * MOVE_X);
+			X = xVal;
+			Y = yVal;
+			RADIUS = radius;
+			IS_HUNTED = false;
+			DISTANCE_FROM_LION = 0;
+		}
+
+		public override void Draw(Graphics g)
+		{
+			g.DrawIcon(icon, (int)X, (int)Y);
+		}
+
+		/// <summary>
+		/// moving forword and change direction when hits a wall.
+		/// saves the distance between the lion and the animel in each step of the animel.
+		/// </summary>
+		/// <param name="a">Lion</param>
+		public override void Move(Animel a)
+		{
+			X = X + MOVE_X;
+			Y = Y + MOVE_Y;
+			if (X >= 1000 - radius * 2 || X <= 0 + radius * 2)
+				MOVE_X = -MOVE_X;
+			if (Y >= 500 - radius * 2 || Y <= 0 + radius * 2)
+				MOVE_Y = -MOVE_Y;
+			//calculate distance
+			SetDistFromLion(a);
+		}
+	}
+
+	public class AnimelList
+	{
+		protected ArrayList animels;
+
+		public AnimelList()
+        {
+			animels = new ArrayList();
+        }
+		
+		public int Count()
+        {
+			return animels.Count;
+        }
+
+		public Hunted Get()
+        {
+			if(animels.Count != 0)
+				return (Hunted)animels[0];
+			return null;
+        }
+
+		public void Add(Hunted animel)
+        {
+			animels.Add(animel);
+        }
+
+		public void Remove()
+        {
+			animels.RemoveAt(0);
+        }
+
+		public void DrawAll(Graphics g)
+		{
+            animels.Sort();
+            for (int i = 0; i < animels.Count; i++)
+				((Hunted)animels[i]).Draw(g);
+		}
+
+		/// <summary>
+		/// moves all the hunted objects.
+		/// must come before the lion's movement.
+		/// </summary>
+		/// <param name="lion"></param>
+		public void MoveAll(Animel lion)
+		{
+			if(animels.Count != 0 )
+				if (((Hunted)animels[0]).IS_HUNTED)
+					Remove();
+			for (int i = 0; i < animels.Count; i++)
+			{
+				((Hunted)animels[i]).Move(lion);
+			}
+		}
+
+        //TODO: implement sort function.
+    }
+
+
+	///
+	/*public class AnimelList
+	{
+		protected SortedList Animels;
+
+		public AnimelList()
+		{
+			Animels = new SortedList();
+		}
+
+		public void Add(Hunted animel)
+        {
+			Animels.Add(animel);
+        }
+
+		public int NextIndex
+		{
+			get
+			{
+				return Animels.Count;
+			}
+		}
+
+		public void Remove(int element)
+		{
+			if (Animels.Count != 0)
+			{
+				Animels.RemoveAt(0);
+			}
+		}
+
+		public void DrawAll(Graphics g)
+		{
+			for (int i = 0; i < Animels.Count; i++)
+				((Hunted)Animels[i]).Draw(g);
+		}
+
+		public void MoveAll(Animel lion)
+        {
+			for (int i = 0; i < Animels.Count; i++)
+				((Hunted)Animels[i]).Move(lion);
+        }
+
+	}*/
 }
--- Form1.cs
+++ Form1.cs
@@ -24,13 +24,14 @@ namespace Safari_app
         //0 for rabbit    1 for giraffe
         int which_animel = -1;
         Lion l;
-        Rabbit r;
+        Hunted animel;
+        AnimelList hunted_animels;
 
         public Form1()
         {
             InitializeComponent();
             l = new Lion(500, 250);
-            r = new Rabbit(300, 300);
+            hunted_animels = new AnimelList();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -51,43 +52,54 @@ namespace Safari_app
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            /*MouseEventArgs me = (MouseEventArgs)e;
+            MouseEventArgs me = (MouseEventArgs)e;
             Point coordinates = me.Location;
-            l = new Lion(coordinates.X, coordinates.Y);*/
+            switch (which_animel)
+            {
+                case 0 :    //create rabbit
+                    animel = new Rabbit(coordinates.X, coordinates.Y);
+                    hunted_animels.Add(animel);
+                    break;
+                case 1:     //create giraffe
+                    animel = new Giraffe(coordinates.X, coordinates.Y);
+                    hunted_animels.Add(animel);
+                    break;
+                default:
+                    break;
+            }
+            
         }
 
+        //Rabbit selector
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             which_animel = 0;
         }
 
+        //Giraffe selector
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
             which_animel = 1;
         }
 
+        //draw all objects to the screen
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            if(r != null)   /////////////////////addad for test
-                r.Draw(g);
+
+            hunted_animels.DrawAll(g);
             l.Draw(g);
         }
 
         //moves all the objects on screen according to timer.
         private void moveTimerEvent(object sender, EventArgs e)
         {
-            if (r != null) ///////////////////////added for test
-            {
-                r.Move();
-                if (r.IS_HUNTED == true)
-                {
+            l.Move(hunted_animels.Get());
+            if(hunted_animels.Count() != 0)
+                if (hunted_animels.Get().IS_HUNTED)
                     checkBox1.Checked = false;
-                    r = null;
-                }
-            }
-            l.Move(r);
+            hunted_animels.MoveAll(l);
             this.Refresh();
 
         }
